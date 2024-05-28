ee1cc37b733e3c9ab0004c865f8d84fbf5569afc
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-05-28 18:35:21 -0500

    Pantalla de inicio hecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PublicLayout from './layouts/PublicLayout';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
@@ -7,8 +8,10 @@ function App() {
 		<>
 			<Router>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="*" element={<NotFound />} />
+					<Route path="/" element={<PublicLayout />}>
+						<Route index element={<Home />} />
+						<Route path="*" element={<NotFound />} />
+					</Route>
 				</Routes>
 			</Router>
 		</>
